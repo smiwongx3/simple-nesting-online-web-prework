8d8a8c449d2c691afa4115af7c6f663844f076db
Samantha Wong <smiwong920@gmail.com>
2019-02-03 21:37:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,10 +73,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -91,7 +87,8 @@ def adding_matz
         }
      }
 
-programmer_hash[]
+programmer_hash[:yukihiro_matsumoto][:known_for => "Ruby", :languages => ["LISP", "C"]]
+programmer_hash
     
 end
 
